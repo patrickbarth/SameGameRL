079844dbdf5f8312883d3ea9c97aa3dfe182dd7b
patrickbarth <20340112+patrickbarth@users.noreply.github.com>
2025-07-24 20:29:11 +0200

    Some more weird git things

---
+++ samegamerl/evaluation/visualize_agent.py
@@ -0,0 +1,49 @@
+from copy import deepcopy
+from math import ceil
+import pygame
+import sys
+
+from samegamerl.agents.dqn_agent import DqnAgent
+from samegamerl.game.View import View
+from samegamerl.game.game_params import NUM_COLS, NUM_ROWS, SCREEN_HEIGHT, SCREEN_WIDTH
+from samegamerl.environments.samegame_env import SameGameEnv
+
+
+def ini_visualization(game):
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Flight Game - AI playing")
+
+    view = View(game)
+
+    return screen, view
+
+
+def play_eval_game(agent: DqnAgent, visualize=False, waiting_time=50):
+    env = SameGameEnv()
+    obs = env.reset()
+    rounds = 0
+    if visualize:
+        screen, view = ini_visualization(env.game)
+    done = env.done
+    agent.model.eval()
+    cur_epsilon = agent.epsilon
+    agent.epsilon = 0
+
+    while (not done) and rounds < ceil(NUM_COLS * NUM_ROWS / 5):
+        move, values = agent.act_eval(obs)
+        if visualize:
+            screen.fill((255, 255, 255))
+            board = deepcopy(env.game.get_board())
+            clicked_tile = env._to_2d(move)
+            board[clicked_tile[0]][clicked_tile[1]] = 6
+            view.draw_text(screen, board, values.tolist())
+            pygame.display.flip()
+            pygame.time.wait(waiting_time)
+        obs, reward, done, _ = env.step(move)
+        print(reward)
+        rounds += 1
+    agent.model.train()
+    agent.epsilon = cur_epsilon
+    pygame.quit()
+    return env.game.left
---
+++ samegamerl/training/train.py
@@ -0,0 +1,81 @@
+from samegamerl.environments.samegame_env import SameGameEnv
+from samegamerl.agents.dqn_agent import DqnAgent
+from samegamerl.evaluation.plot_helper import plot_results
+from tqdm import tqdm
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+from samegamerl.evaluation.visualize_agent import play_eval_game
+
+# boundaries
+batch_size = 256
+n_episodes = 30_000
+max_steps = 20  # half of cells on the field is a reasonable value
+update_target_freq = 10
+report_freq = 100
+initial_update_done = n_episodes // 2
+visualize_freq = 5000
+
+# hyperparameters
+learning_rate = 0.0001
+start_epsilon = 1.0
+epsilon_decay = start_epsilon / n_episodes
+final_epsilon = 0.1
+
+env = SameGameEnv()
+agent = DqnAgent(
+    learning_rate=learning_rate,
+    initial_epsilon=start_epsilon,
+    epsilon_decay=epsilon_decay,
+    final_epsilon=final_epsilon,
+    gamma=0,
+    batch_size=batch_size,
+)
+scheduler = ReduceLROnPlateau(agent.opt, "min", patience=100, cooldown=10)
+
+total_reward = 0
+loss = 0
+results = []
+
+# agent.load()
+
+for episode in tqdm(range(n_episodes)):
+    obs = env.reset()
+
+    for step in range(max_steps):
+        action = agent.act(obs)
+
+        next_obs, reward, done, _ = env.step(action)
+
+        agent.remember(obs, action, reward, next_obs, done)
+        obs = next_obs
+        total_reward += reward
+
+        if done:
+            break
+
+    cur_loss = agent.learn()
+    loss += cur_loss
+    # agent.decrease_epsilon()
+
+    # if episode % update_target_freq == 0:
+    #    agent.update_target_model()
+
+    # if episode == initial_update_done:
+    # agent.gamma = 0.8
+    # agent.epsilon = 1
+
+    if episode % report_freq == report_freq - 1:
+        results.append(float(loss))
+        scheduler.step(float(loss))
+        # print(f"Episode {episode+1} - Reward: {total_reward:.2f} - Epsilon: {agent.epsilon:.3f} - Loss: {loss:.5f} ")
+        # total_reward = 0
+        loss = 0
+
+    if episode % visualize_freq == visualize_freq - 1:
+        print(scheduler.get_last_lr())
+        play_eval_game(agent, visualize=True, waiting_time=500)
+
+
+agent.save()
+
+plot_results(results, interval=1)
